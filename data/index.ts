065ad85f143676adfd759baa2a10e7f2aa2d1d47
Aayush1904 <asagrawal612@gmail.com>
2024-05-22 11:49:25 +0530

    ...

---
+++ data/index.ts
@@ -0,0 +1,217 @@
+export const navItems = [
+  { name: "About", link: "#about" },
+  { name: "Projects", link: "#projects" },
+  { name: "Testimonials", link: "#testimonials" },
+  { name: "Contact", link: "#contact" },
+];
+
+export const gridItems = [
+  {
+    id: 1,
+    title: "I prioritize client collaboration, fostering open communication ",
+    description: "",
+    className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+    imgClassName: "w-full h-full",
+    titleClassName: "justify-end",
+    img: "/b1.svg",
+    spareImg: "",
+  },
+  {
+    id: 2,
+    title: "I'm very flexible with time zone communications",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 3,
+    title: "My tech stack",
+    description: "I constantly try to improve",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-center",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 4,
+    title: "Tech enthusiast with a passion for development.",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "/grid.svg",
+    spareImg: "/b4.svg",
+  },
+
+  {
+    id: 5,
+    title: "Currently building a Hotel Management System",
+    description: "The Inside Scoop",
+    className: "md:col-span-3 md:row-span-2",
+    imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+    titleClassName: "justify-center md:justify-start lg:justify-center",
+    img: "/b5.svg",
+    spareImg: "/grid.svg",
+  },
+  {
+    id: 6,
+    title: "Do you want to start a project together?",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+    img: "",
+    spareImg: "",
+  },
+];
+
+export const projects = [
+  {
+    id: 1,
+    title: "AImage - Image utility with style",
+    des: "Explore the designs and amazing features of our appliction just by clicking over it.",
+    img: "/Users/aayushagarwal/nextportfolio/public/AImage.jpeg",
+    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/fm.svg"],
+    link: "https://a-image.vercel.app",
+  },
+  {
+    id: 2,
+    title: "Farmer application",
+    des: "Farmers can aslo showcase thier products only through our incredeble web app.",
+    img: "/Users/aayushagarwal/nextportfolio/public/E-Commerce.jpeg",
+    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/stream.svg", "/c.svg"],
+    link: "https://aayush1904.github.io/FarmingWeb/",
+  },
+  {
+    id: 3,
+    title: "Expense Tracker",
+    des: "Track your expenses with our utility.",
+    img: "/p3.svg",
+    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/c.svg"],
+    link: "expense-tracker-tan-one.vercel.app",
+  },
+  {
+    id: 4,
+    title: "AI-saas application for image utility",
+    des: "Image utility for awesome image editing.",
+    img: "/Users/aayushagarwal/nextportfolio/public/Imaginification.jpeg",
+    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/three.svg", "/gsap.svg"],
+    link: "aimage-zeta.vercel.app",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Collaborating with Aayush was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Aayush's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Aayush is the ideal partner.",
+    name: "xyz",
+    title: "Director of xyz Technologies",
+  },
+  {
+    quote:
+      "Collaborating with Aayush was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Aayush's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Aayush is the ideal partner.",
+    name: "xyz",
+    title: "Director of xyz Technologies",
+  },
+  {
+    quote:
+      "Collaborating with Aayush was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Aayush's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Aayush is the ideal partner.",
+    name: "xyz",
+    title: "Director of xyz Technologies",
+  },
+  {
+    quote:
+      "Collaborating with Aayush was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Aayush's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Aayush is the ideal partner.",
+    name: "xyz",
+    title: "Director of xyz Technologies",
+  },
+  {
+    quote:
+      "Collaborating with Aayush was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Aayush's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Aayush is the ideal partner.",
+    name: "xyz",
+    title: "Director of xyz Technologies",
+  },
+];
+
+export const companies = [
+  {
+    id: 1,
+    name: "cloudinary",
+    img: "/cloud.svg",
+    nameImg: "/cloudName.svg",
+  },
+  {
+    id: 2,
+    name: "appwrite",
+    img: "/app.svg",
+    nameImg: "/appName.svg",
+  },
+  {
+    id: 3,
+    name: "HOSTINGER",
+    img: "/host.svg",
+    nameImg: "/hostName.svg",
+  },
+  {
+    id: 4,
+    name: "stream",
+    img: "/s.svg",
+    nameImg: "/streamName.svg",
+  },
+  {
+    id: 5,
+    name: "docker.",
+    img: "/dock.svg",
+    nameImg: "/dockerName.svg",
+  },
+];
+
+export const workExperience = [
+  {
+    id: 1,
+    title: "Frontend Engineer Intern",
+    desc: "Assisted in the development of a web-based platform using React.js, enhancing interactivity.",
+    className: "md:col-span-2",
+    thumbnail: "/exp1.svg",
+  },
+  {
+    id: 2,
+    title: "Machine Learning intern",
+    desc: "Designed and developed mobile app for both iOS & Android platforms using React Native.",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/exp2.svg",
+  },
+  {
+    id: 3,
+    title: "Freelance web Dev Project",
+    desc: "Led the dev of a web app for a client, from initial concept to deployment on google.",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/exp3.svg",
+  },
+  {
+    id: 4,
+    title: "Lead Frontend Developer",
+    desc: "Developed and maintained user-facing features using modern frontend technologies.",
+    className: "md:col-span-2",
+    thumbnail: "/exp4.svg",
+  },
+];
+
+export const socialMedia = [
+  {
+    id: 1,
+    img: "/git.svg",
+  },
+  {
+    id: 2,
+    img: "/twit.svg",
+  },
+  {
+    id: 3,
+    img: "/link.svg",
+  },
+];
---
+++ sentry.edge.config.ts
@@ -0,0 +1,16 @@
+// This file configures the initialization of Sentry for edge features (middleware, edge routes, and so on).
+// The config you add here will be used whenever one of the edge features is loaded.
+// Note that this config is unrelated to the Vercel Edge Runtime and is also required when running locally.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://e3d007c4dab2c39655bb64852a5ac4d7@o4507294619926528.ingest.us.sentry.io/4507294622482432",
+
+  // Adjust this value in production, or use tracesSampler for greater control
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
